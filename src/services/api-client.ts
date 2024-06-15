c9b0193e223fb58e90ada0cb8dd2c1bebe1d1d4b
Slim-Shary <shahryarmon@gmail.com>
2024-06-14 17:46:18 -0700

    Read API key from environment variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '346a5eb12de540e290bd58da53c3519b',
+    key: process.env.RAWG_API_KEY,
   },
 });
 
